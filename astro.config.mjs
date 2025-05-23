2a48ae9573d30908986ecc7a12fb9842c85287d0
Asaad47 <75583625+Asaad47@users.noreply.github.com>
2025-05-23 15:17:55 +0300

    Fix config for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://asaad47.github.io', // Replace with your GitHub username
-  base: '/nikola-tesla', // Replace with your repository name
+  site: 'https://asaad47.github.io',
+  base: '/nikola-tesla',
   vite: {
     plugins: [tailwindcss()]
   },
@@ -24,7 +22,5 @@ export default defineConfig({
       cssVariable: "--font-geist",
       fallbacks: ["Inter", "sans-serif"],
     }]
-  },
-
-  adapter: vercel()
+  }
 });
